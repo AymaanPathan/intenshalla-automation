48835f3267ec3bbe0b2bfc3ac8ec4004b5c17f08
Afzal-24 <aymaanpathan5@gmail.com>
2024-08-22 17:21:12 +0530

    add closing window

--- backend/automation.js
+++ backend/automation.js
@@ -1,6 +1,20 @@
 const puppeteer = require("puppeteer-extra");
 const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
+const category = "web development";
+const cover = `I am writing to express my interest in the internship opportunity at Your as advertised on Internshala. As a passionate and dedicated Full-Stack Developer currently in my third year of BCA, I am eager to apply my technical skills and knowledge in a professional environment where I can contribute meaningfully while gaining invaluable experience.
+
+Over the course of my studies and personal projects, I have developed a robust skill set that includes HTML, CSS, JavaScript, React, Node.js, Express, and MongoDB. I have applied these technologies in various projects, such as creating an e-commerce website with payment integration, developing a library management system, and building an AI chatbot. These experiences have not only strengthened my technical expertise but also honed my ability to work effectively in both individual and team settings.
+
+What excites me most about joining is the opportunity to work in a dynamic and innovative environment where I can further develop my skills and contribute to impactful projects. I am particularly drawn to the chance to learn from experienced professionals and to apply my knowledge in ways that benefit the company.
+
+I am confident that my enthusiasm, strong technical foundation, and willingness to learn make me a valuable addition to your team. I am eager to bring my skills to this company and contribute to the success of your projects.
+
+Thank you for considering my application. I look forward to the opportunity to discuss how I can contribute to your team and grow as a professional during this internship.
+
+Sincerely,
+
+Aymaan Pathan`;
 puppeteer.use(StealthPlugin());
 
 (async () => {
@@ -60,9 +74,13 @@ puppeteer.use(StealthPlugin());
     await page.click("#select_category_chosen");
     await page.waitForSelector("#select_category_chosen > ul > li > input");
     await delay(3000);
-    await page.type("#select_category_chosen > ul > li > input", " front ", {
-      delay: 150,
-    });
+    await page.type(
+      "#select_category_chosen > ul > li > input",
+      ` ${category} `,
+      {
+        delay: 150,
+      }
+    );
     await page.keyboard.press("Enter");
 
     await delay(3000);
@@ -75,7 +93,7 @@ puppeteer.use(StealthPlugin());
       ".container-fluid.individual_internship.easy_apply.button_easy_apply_t.visibilityTrackerItem"
     );
 
-    for (let i = 2; i < internshipElements.length; i++) {
+    for (let i = 13; i < internshipElements.length; i++) {
       const element = internshipElements[i];
 
       // Get internship name
@@ -93,9 +111,26 @@ puppeteer.use(StealthPlugin());
       await delay(1000);
       await page.click("#continue_button");
       delay(1000);
-      await page.waitForSelector("#cover_letter");
-      await page.click("#cover_letter");
-      await page.type("cover_letter", "hi");
+      await page.waitForSelector(
+        "#cover_letter_holder > div.ql-editor.ql-blank"
+      );
+      await page.type("#cover_letter_holder > div.ql-editor.ql-blank", cover);
+
+      const textarea1 = await page.$('textarea[name^="text_"]');
+      if (textarea1) {
+        await textarea1.type("Your text here");
+      }
+
+      delay(3000);
+      await page.waitForSelector(
+        ".submit_button_container.easy_apply_footer #submit"
+      );
+
+      await page.click(".submit_button_container.easy_apply_footer #submit");
+      delay(3000);
+
+      await page.waitForSelector("#not-interested");
+      await page.click("#not-interested");
     }
   } catch (error) {
     console.error("Error:", error);
